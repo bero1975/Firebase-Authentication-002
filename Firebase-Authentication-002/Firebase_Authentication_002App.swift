1d73e7e09f82970c5bfef72b8cb658cb3f2686ff
Ben Robertson <escoffierdesign@icloud.com>
2022-02-02 18:33:30 +0000

    Initial Commit

---
+++ Firebase-Authentication-002/Firebase_Authentication_002App.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_Authentication_002App.swift
+//  Firebase-Authentication-002
+//
+//  Created by Ben Robertson on 02/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_Authentication_002App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
